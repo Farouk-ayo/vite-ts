438be619f0fa728bbe89b531c32884192aa12b3b
Farouk-ayo <mustaphafarouk41@gmail.com>
2023-10-19 13:20:15 +0100

    done

--- react-ts-vite/src/App.tsx
+++ react-ts-vite/src/App.tsx
@@ -2,6 +2,7 @@ import Heading from "./components/Heading";
 import { Section } from "./components/Section";
 import Counter from "./components/Counter";
 import { useState } from "react";
+import List from "./components/List";
 
 function App() {
   const [count, setCount] = useState<number>(1);
@@ -11,6 +12,12 @@ function App() {
       <Heading title="Hello" />
       <Section title="different title">This is my section</Section>
       <Counter setCount={setCount}>Count is {count}</Counter>
+      <List
+        items={["☕ coffee", "👅 Tacos", "💻 code"]}
+        render={(item: string) => {
+          return <span className="gold">{item}</span>;
+        }}
+      />
     </>
   );
 }
--- react-ts-vite/src/components/List.tsx
+++ react-ts-vite/src/components/List.tsx
@@ -1,1 +1,19 @@
 import { ReactNode } from "react";
+
+interface ListProps<T> {
+  items: T[];
+  render: (item: T) => ReactNode;
+}
+
+// const List = <T extends React.ReactNode>({ items, render }: ListProps<T>) => {
+// or
+const List = <T,>({ items, render }: ListProps<T>) => {
+  return (
+    <ul>
+      {items.map((item, index) => (
+        <li key={index}>{render(item)}</li>
+      ))}
+    </ul>
+  );
+};
+export default List;
